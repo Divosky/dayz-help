f5c484a024e486ea9d01cb6b44599ad49f03c1f8
Divosky <divoskyy@gmail.com>
2018-10-04 14:05:12 +0200

    Improve image generation && Change form layout

--- src/views/Generator.vue
+++ src/views/Generator.vue
@@ -1,15 +1,23 @@
 <template>
+  <!-- eslint-disable -->
+  <!-- TODO: Clean the code, rewrite the whole view if time and money make it possible -->
   <main class="main">
     <section class="page__header">
       <h3 class="page__title">Generator</h3>
     </section>
     <section class="generator">
+      <div class="alert alert--info">
+        <p>
+          To Generate images we use canvas element, therefore to have it displayed properly
+          we recommend using a device with bigger viewport.
+        </p>
+      </div>
       <form class="form">
         <div class="form-group">
           <h4 class="form__group__title">Choose Image Type</h4>
           <label for="imageType">
             <select class="input input--select" v-model="imageType" id="imageType">
-              <option value="0" selected>Type</option>
+              <option disabled value="">Choose Type</option>
               <option value="signature">Signature</option>
               <option value="userbar">Userbar</option>
             </select>
@@ -43,7 +51,7 @@
         </div>
       </form>
       <div class="preview">
-        <canvas v-show="imageType == 'signature'" v-message="{userNickname, imageBackground, imageType}" width="500" height="250"></canvas>
+        <canvas v-show="imageType == 'signature'" v-message="{userNickname, userGang, imageBackground, imageType}" width="500" height="250"></canvas>
         <canvas v-show="imageType == 'userbar'" v-message="{userNickname, imageBackground, imageType}" width="350" height="19"></canvas>
       </div>
     </section>
@@ -65,19 +73,32 @@ export default {
     message: (canvasElement, binding) => {
       const ctx = canvasElement.getContext('2d');
       const background = new Image();
-      background.src = `https://i.imgur.com/${binding.value.imageBackground || 'DOhEpbJ' }.png`;
-      background.onload = () => {
-        ctx.drawImage(background, 0, 0);
-        if (binding.value.imageType == 'signature') {
-          ctx.fillStyle = 'pink';
-          ctx.font = '20px Georgia';
-          ctx.fillText(binding.value.userNickname, 10, 50);
-        } else if(binding.value.imageType == 'userbar') {
+      background.src = `https://i.imgur.com/${binding.value.imageBackground || 'DOhEpbJ'}.png`;
+      if (binding.value.imageType === 'signature') {
+        background.onload = () => {
+          ctx.drawImage(background, 0, 0);
+          if (binding.value.imageBackground === 'DOhEpbJ') {
+            ctx.fillStyle = 'yellow';
+            ctx.font = '14px Georgia';
+            ctx.fillText(`Nickname: ${binding.value.userNickname}`, 40, 65);
+            ctx.fillText(`Gang: ${binding.value.userGang}`, 40, 85);
+          } else if (binding.value.imageBackground === 'h4Dw74f') {
+            ctx.fillStyle = '#44d7ff';
+            ctx.font = '12px Georgia';
+            ctx.fillText(`Nickname: ${binding.value.userNickname}`, 25, 50);
+            ctx.fillText(`Gang: ${binding.value.userGang}`, 25, 80);
+          }
+        };
+      } else if (binding.value.imageType === 'userbar') {
+        // Not planning to add more of them in the future
+        background.src = 'https://i.imgur.com/xEYsg7E.png';
+        background.onload = () => {
+          ctx.drawImage(background, 0, 0);
           ctx.fillStyle = 'yellow';
           ctx.font = '13px Arial';
           ctx.fillText(binding.value.userNickname, 5, 15);
-        }  
-      };
+        };
+      }
     },
   },
 };
@@ -86,9 +107,33 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/scss/_extends.scss';
 
+.alert {
+  margin: var(--base-margin);
+  padding: var(--base-padding);
+  border-radius: var(--border-radius);
+
+  &--info {
+    background: var(--primary);
+    color: var(--base-bg-color)
+  }
+}
+
+.preview {
+  display: flex;
+  justify-content: center;
+  margin-top: calc(var(--base-margin) * 3);
+}
+
+.form {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 2fr));
+  grid-gap: 1rem;
+}
+
 .input {
   &:not(:last-child) {
     margin-right: calc(var(--base-margin) * .3);
+    margin-bottom: calc(var(--base-margin) * .3);
   }
 
   &__image-background {
@@ -123,5 +168,11 @@ export default {
 
 .generator {
   @extend %content-container;
+}
+
+@media (min-width: 400px) {
+  .alert {
+    display: none;
+  }
 }
 </style>
